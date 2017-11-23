bc7bcbf4816045e0abc1b131c61f27e1cc181c08
Angel Arciniega <choy_1_10@hotmail.es>
2017-11-22 18:51:39 -0800

    my first clone

---
+++ PoliTracker/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+using Android.Gms.Maps;
+using System;
+using Android.Gms.Maps.Model;
+
+namespace PoliTracker
+{
+    [Activity(Label = "@string/app_name")]
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
+    {
+        static readonly string TAG = "X:" + typeof(MainActivity).Name;
+        private GoogleMap GMap;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            //Set your main view here
+            SetContentView(Resource.Layout.main);
+            SetUpMap();
+            Log.Debug(TAG, "MainActivity is loaded.");
+        }
+        private void SetUpMap()
+        {
+            if (GMap == null)
+            {
+                FragmentManager.FindFragmentById<MapFragment>(Resource.Id.googlemap).GetMapAsync(this);
+            }
+        }
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            GMap = googleMap;
+            LatLng latlng = new LatLng(Convert.ToDouble(13.0291), Convert.ToDouble(80.2083));
+            CameraUpdate camera = CameraUpdateFactory.NewLatLngZoom(latlng, 15);
+            GMap.MoveCamera(camera);
+            MarkerOptions options = new MarkerOptions().SetPosition(latlng).SetTitle("Chennai");
+            GMap.AddMarker(options);
+        }
+    }
+}
+
---
+++ PoliTracker/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("PoliTracker.MainApplication, PoliTracker, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md524baacf6aa02f2ce19b52c6fe848c64e.MainApplication.class, md524baacf6aa02f2ce19b52c6fe848c64e.MainApplication.__md_methods);
+		
+	}
+}
